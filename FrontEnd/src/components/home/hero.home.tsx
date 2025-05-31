319e9666898b87c90f6d58aeac52c656820a2b03
Gabriel Antônio <gabriel.antonio.dlm@gmail.com>
2025-05-31 20:22:12 -0300

    update: post release minor updates

--- FrontEnd/src/components/home/hero.home.tsx
+++ FrontEnd/src/components/home/hero.home.tsx
@@ -7,11 +7,7 @@ export default function Hero() {
 			<div className="flex flex-col sm:w-3/4 md:7/10 lg:w-3/5 xl:w-2/5 py-10">
 				<h1 className="font-extrabold text-[60px]">One more friend</h1>
 				<h2 className="font-bold text-[46px]">Thousands more fun!</h2>
-				<p className="py-10">
-					Having a pet means you have more joy, a new friend, a happy
-					person who will always be with you to have fun. We have 200+
-					different pets that can meet your needs!
-				</p>
+				<p className="py-10">Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!</p>
 				<div className="flex flex-row gap-5">
 					<LightButton text="View Intro" icon="play" />
 					<DarkButton text="Explore Now" />
